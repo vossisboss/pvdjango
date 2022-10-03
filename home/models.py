2656ea145d98c8cff6d3baf6ec528fefc7574bc7
vossisboss <vossmk@gmail.com>
2022-10-03 14:14:37 -0400

    Added fix for referring to the custom image.

--- home/models.py
+++ home/models.py
@@ -4,18 +4,18 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+from wagtail import images
 
 
 class HomePage(Page):
     summary = RichTextField(blank=True)
     main_image = models.ForeignKey(
-        "wagtailimages.Image",
+        images.get_image_model_string(),
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
         related_name="+",
     )
-
     content_panels = Page.content_panels + [
         FieldPanel('summary'),
         FieldPanel('main_image'),
