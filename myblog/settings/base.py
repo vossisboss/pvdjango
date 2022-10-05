394c86fc4c7865c243cab1bb1f2509e3a69ee6b0
vossisboss <vossmk@gmail.com>
2022-10-05 15:41:09 -0400

    Added dummy translator.

--- myblog/settings/base.py
+++ myblog/settings/base.py
@@ -137,6 +137,10 @@ WAGTAIL_CONTENT_LANGUAGES = LANGUAGES = [
     ("fr", "French"),
 ]
 
+WAGTAILLOCALIZE_MACHINE_TRANSLATOR = {
+    "CLASS": "wagtail_localize.machine_translators.dummy.DummyTranslator",
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
