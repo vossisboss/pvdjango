da3b803c6cb59b28a9085d80c183ed4cf25e4450
vossisboss <vossmk@gmail.com>
2022-10-05 16:24:35 -0400

    Removing code typos from step 2.

--- home/models.py
+++ home/models.py
@@ -1,24 +1,7 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    summary = RichTextField(blank=True)
-    main_image = models.ForeignKey(
-        "wagtailimages.Image",
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name="+",
-    )
-
-    content_panels = Page.content_panels + [
-        FieldPanel('summary'),
-        FieldPanel('main_image'),
-
-    ]
-
-
+    pass
