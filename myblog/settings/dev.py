a208ee11c0ca673d37cb05e08341d3f2ffa9df9e
vossisboss <vossmk@gmail.com>
2022-10-13 21:22:49 -0400

    Updating step code.

--- myblog/settings/dev.py
+++ myblog/settings/dev.py
@@ -9,6 +9,8 @@ SECRET_KEY = "django-insecure-p@ty54s(-j8qjphym5txsr0uh2=%2hj&^t6a7-b06$4-^ba&eh
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io']
+
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
