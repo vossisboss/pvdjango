533ceb9de11dd1efa0b912d3a5bc8a1eac5290c7
vossisboss <vossmk@gmail.com>
2022-10-14 14:36:57 -0400

    Fixed duplicate menus issue.

--- navigation/templatetags/navigation_tags.py
+++ navigation/templatetags/navigation_tags.py
@@ -6,6 +6,22 @@ from wagtail.models import Locale
 register = template.Library()
 # https://docs.djangoproject.com/en/3.2/howto/custom-template-tags/
 
+def filter_nav_for_locale(cls, locale):
+    menu_items = None
+
+    try:
+        menu_items = cls.objects.filter(locale=locale)
+    except cls.DoesNotExist:
+        pass
+
+    if not menu_items:
+        try:
+            menu_items = cls.objects.filter(locale=default_language())
+        except cls.DoesNotExist:
+            pass
+
+    return menu_items
+
 @register.inclusion_tag("navigation/footer.html", takes_context=True)
 def get_footer_text(context):
     footer_text = ""
@@ -20,9 +36,9 @@ def get_footer_text(context):
 def get_main_navigation(context):
     request = context["request"]
     locale = Locale.objects.get(language_code=request.LANGUAGE_CODE)
-    menu_items = MainNavigation.objects.get(locale=locale)
+    print(locale)
+    menu_items = filter_nav_for_locale(MainNavigation, locale)
         
     return {
-        "menu_items": menu_items,
+        "menu_items": list(menu_items),
     }
-
